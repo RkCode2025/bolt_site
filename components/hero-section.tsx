1825a109eb214a6a7370f1cceb2f9b3f39badc61
Syphax <ss3240069@gmail.com>
2025-11-13 11:13:23 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import profilePic from 'Miyamoto Musashi.jpg'; // ✅ Make sure image is in /public folder
+import profilePic from '𝘔𝘪𝘺𝘢𝘮𝘰𝘵𝘰 𝘔𝘶𝘴𝘢𝘴𝘩𝘪.jpg'; // ✅ Make sure image is in /public folder
 
 export function HeroSection() {
   return (
