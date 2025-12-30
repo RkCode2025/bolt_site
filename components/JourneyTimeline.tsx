6bea5c3a2f8502aa76c630fa406eb70ea9a6eef6
Syphax <ss3240069@gmail.com>
2025-12-30 12:23:37 +0530

    Update JourneyTimeline.tsx

--- components/JourneyTimeline.tsx
+++ components/JourneyTimeline.tsx
@@ -24,7 +24,7 @@ export default function Experience() {
       company: "Fiberr.co",
       date: "Nov 2025 – Present",
       // Updated to match your renamed file
-      logo: "/blob/main/fib.png", 
+      logo: "/fib.png", 
       githubUrl: "https://github.com/old-droid",
     },
   ];
