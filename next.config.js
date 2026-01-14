2947db66857e52c018a5c7d6cef92becc946bff9
Syphax <ss3240069@gmail.com>
2026-01-14 16:17:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Required for static site hosting
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true, // Required for 'export' to work with next/image
+  },
+  // This ensures trailing slashes are handled correctly in static exports
+  trailingSlash: true, 
 };
 
 module.exports = nextConfig;
